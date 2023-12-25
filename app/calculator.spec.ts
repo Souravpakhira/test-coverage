02255639af8b16e95a158258a612a28a116adf64
Sourav Pakhira <pakhira55@gmail.com>
2023-12-25 12:44:22 +0530

    sub added

--- app/calculator.spec.ts
+++ app/calculator.spec.ts
@@ -1,5 +1,9 @@
 // import { add } from './calculator' 
-import { add } from "./calulator";
+import { add, sub } from "./calulator";
 test('add function', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('sub function', () => {
+  expect(sub(3, 1)).toBe(2);
 });
